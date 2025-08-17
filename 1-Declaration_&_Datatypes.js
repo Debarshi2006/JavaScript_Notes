8f35d43e9e94144ab1144287e29097e40bce6206
Debarshi Das <debarshidas742@gmail.com>
2025-08-17 12:13:33 +0530

    Update 1-Declaration_&_Datatypes.js

--- 1-Declaration_&_Datatypes.js
+++ 1-Declaration_&_Datatypes.js
@@ -22,6 +22,11 @@ console.table([account_email,accountPassword,accountCity,accountState])
 
 // *****************Datatypes*****************
 
+// Depending on the memory allocation and accestion of data , datatypes are divided in two parts.
+//primitive & non-primitive.
+// Primitive datatypes are call by value in nature. In case of using, an copy of the data is shared instead of the memory reference. New changes are saved in the copy.
+
+//---------Primitive---------
 // number
 // bigint
 // string
@@ -29,7 +34,26 @@ console.table([account_email,accountPassword,accountCity,accountState])
 // null => standalone value
 // undefined
 // symbol => to make some element unique
-// object 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
 
 console.log(typeof null) //type object. 
 console.log(typeof undefined) // type undefined
+
+//---------Non-primitive or reference---------
+// array
+// object
+// function
+
+const heros = ["apple", "samsung", "motorola"]
+
+let myObj = {
+    names: "Debarshi",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("Hello world!")
+}
